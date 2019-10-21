3785b19f55559d751466760621bb84c207d228f9
Alex Faria <alexandre.faria@tecnico.ulisboa.pt>
2019-10-21 10:05:04 +0100

    Added PCS project

---
+++ MSDAD/PCS/PCS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCS
+{
+    class PCS
+    {
+        RemotePCSObject remotePCS;
+        static void Main(string[] args)
+        {
+            TcpChannel channel = new TcpChannel(10000);
+            ChannelServices.RegisterChannel(channel, true);
+            RemotePCSObject mo = new RemotePCSObject();
+            RemotingServices.Marshal(mo, "Server", typeof(RemotePCSObject));
+
+        }
+    }
+}
