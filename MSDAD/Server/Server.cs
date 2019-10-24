f6fdce2de4411f473dcaaaa4f166cd80a9520b4e
Alex Faria <alexandre.faria@tecnico.ulisboa.pt>
2019-10-24 20:22:08 +0100

    Add command line argument parser to client and server

--- MSDAD/Server/Server.cs
+++ MSDAD/Server/Server.cs
@@ -1,4 +1,5 @@
 ﻿using Commontypes;
+using System;
 using System.Runtime.Remoting;
 using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Tcp;
@@ -9,19 +10,30 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            TcpChannel channel = new TcpChannel(8086);
+            if (args.Length <= 0)
+            {
+                Console.WriteLine("usage: ./Server.exe <server_id> <url> <max_faults> <max_delay> <min_delay>");
+                Console.WriteLine("<enter> para sair...");
+                Console.ReadLine();
+                return;
+            }
+
+            string server_id = args[0];
+            string url = args[1];
+            int max_faults = Int32.Parse(args[2]);
+            int min_delay = Int32.Parse(args[3]);
+            int max_delay = Int32.Parse(args[4]);
+            Uri uri = new Uri(url);
+
+            Console.WriteLine($"server: {server_id} {url} {max_faults} {max_delay} {min_delay}");
+
+            TcpChannel channel = new TcpChannel(uri.Port);
             ChannelServices.RegisterChannel(channel, true);
-            RemoteServerObject mo = new RemoteServerObject();
-            RemotingServices.Marshal(mo,
-            "Server",
-            typeof(RemoteServerObject));
-           /* RemotingConfiguration.RegisterWellKnownServiceType(
-                typeof(RemoteServerObject),
-                "Server",
-                WellKnownObjectMode.Singleton);
-            */
-            System.Console.WriteLine("<enter> para sair...");
-            System.Console.ReadLine();
+            RemoteServerObject remoteServerObj = new RemoteServerObject();
+            RemotingServices.Marshal(remoteServerObj, uri.LocalPath.Trim('/'), typeof(RemoteServerObject));
+
+            Console.WriteLine("<enter> para sair...");
+            Console.ReadLine();
         }
     }
 }
