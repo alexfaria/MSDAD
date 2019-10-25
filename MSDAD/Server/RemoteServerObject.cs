ef2f8e7c7dcf87c1ea62f64fbdce559cd8dc597d
rafampinto <rafapinto23@hotmail.com>
2019-10-25 12:01:41 +0100

    Added CloseMeeting impl

--- MSDAD/Server/RemoteServerObject.cs
+++ MSDAD/Server/RemoteServerObject.cs
@@ -7,6 +7,7 @@ namespace Server
     class RemoteServerObject : MarshalByRefObject, IServer
     {
         List<Meeting> meetings = new List<Meeting>();
+        List<Location> locations = new List<Location>();
         private int max_faults;
         private int max_delay;
         private int min_delay;
@@ -31,11 +32,10 @@ namespace Server
 
         public void CreateMeeting(Meeting m)
         {
-            bool exist = false;
-            foreach (Meeting meeting in meetings)
-                if (m.Equals(meeting))
-                    exist = true;
-            if (!exist) meetings.Add(m);
+            if (!meetings.Contains(m))
+            {
+                meetings.Add(m);
+            }
         }
 
         public void JoinMeeting(string user, string meetingTopic, Slot slot)
@@ -54,7 +54,40 @@ namespace Server
         public void CloseMeeting(string user, string meetingTopic)
         {
             Meeting meeting = meetings.Find((m1) => m1.topic.Equals(meetingTopic));
-            // ToDo
+            Slot slot = null;
+            List<Room> rooms = null;
+            foreach (Slot s in meeting.slots) // Find the best slot for the meeting
+            {
+                Location location = locations.Find(l => l.name.Equals(s.location));
+                List<Room> free = location.rooms.FindAll(r => !r.booked.Contains(s.date));
+                if (free.Count > 0) // There is a free room
+                    if (slot == null || slot != null && s.participants.Count >= meeting.min_participants && s.participants.Count > slot.participants.Count)
+                    {
+                        slot = s;
+                        rooms = free;
+                    }
+            }
+            if (slot == null)
+            {
+                // meeting.Cancel();
+                return;
+            }
+            Room room = null;
+            foreach (Room r in rooms) // Find the best room for the meeting
+            {
+                if (r.capacity >= slot.participants.Count)
+                {
+                    room = r;
+                    break;
+                }
+                if (r.capacity > room.capacity)
+                    room = r;
+            }
+            room.booked.Add(slot.date); // Book the room
+            if (room.capacity < slot.participants.Count)
+                // If there are more registered participants than the capacity of the selected meeting room
+                slot.participants.RemoveRange(room.capacity - 1, slot.participants.Count - room.capacity);
+            // The meeting is scheduled
         }
 
         public void AddRoom(string location, int capacity, string room_name)
