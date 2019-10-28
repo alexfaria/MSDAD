979850c83fde2d1b1354590661559de48b7bea86
rafampinto <rafapinto23@hotmail.com>
2019-10-28 10:30:59 +0000

    Removed unnecessary code

--- MSDAD/PCS/RemotePCSObject.cs
+++ MSDAD/PCS/RemotePCSObject.cs
@@ -7,13 +7,6 @@ namespace PCS
 {
     class RemotePCSObject : MarshalByRefObject, IPCS
     {
-        List<IServer> serverList;
-        List<IClient> clientList;
-        public RemotePCSObject()
-        {
-            clientList = new List<IClient>();
-            serverList = new List<IServer>();            
-        }
         public void Client(string username, string client_URL, string server_URL, string script_file)
         {
             Console.WriteLine($"starting client: {username} {client_URL} {server_URL} {script_file}");
@@ -23,13 +16,6 @@ namespace PCS
         {
             Console.WriteLine($"starting server: {server_id} {URL} {max_faults} {min_delay} {max_delay}");
             Process.Start(@"Server.exe", $"{server_id} {URL} {max_faults} {min_delay} {max_delay}");
-        }
-        public void AddRoom(string location, int capacity, string room_name)
-        {
-            foreach (IServer server in serverList)
-            {
-                server.AddRoom(location, capacity, room_name);
-            }
         }
     }
 }
