8931ddb0b7e9463c0131bc309b76106deda523b6
Rafampinto <rafapinto23@hotmail.com>
2019-11-02 00:14:58 +0000

    Added ApplicationExceptions to deal with errors handling meetings

---
+++ MSDAD/CommonTypes/InvalidMeetingException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTypes
+{
+    [Serializable]
+    public class InvalidMeetingException : ApplicationException
+    {
+        public string meetingTopic;
+        public InvalidMeetingException(string meetingTopic)
+        {
+            this.meetingTopic = meetingTopic;
+        }
+
+        public InvalidMeetingException(System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context)
+        : base(info, context)
+        {
+            meetingTopic = info.GetString("meetingTopic");
+        }
+
+        public override void GetObjectData(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue("meetintTopic", meetingTopic);
+        }
+    }
+}
---
+++ MSDAD/CommonTypes/UnauthorizedException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTypes
+{
+    [Serializable]
+    public class UnauthorizedException : ApplicationException
+    {
+        public string user;
+        public UnauthorizedException(string user)
+        {
+            this.user = user;
+        }
+
+        public UnauthorizedException(System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context)
+        : base(info, context)
+        {
+            user = info.GetString("user");
+        }
+
+        public override void GetObjectData(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue("user", user);
+        }
+    }
+}
