a6c254b38d562b382dd35c99b06baf80834bedb7
Rafampinto <rafapinto23@hotmail.com>
2019-11-02 00:19:00 +0000

    Added some folders to improve organization of the project

---
+++ MSDAD/CommonTypes/Model/Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CommonTypes
+{
+    [Serializable]
+    public class Location
+    {
+        public string name;
+        public List<Room> rooms;
+        
+        public Location(string name, List<Room> rooms)
+        {
+            this.name = name;
+            this.rooms = rooms;
+        }
+
+    }
+}
